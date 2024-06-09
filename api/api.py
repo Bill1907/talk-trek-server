2cd75e58d1c03491826dfb08085b4f4235fd4e70
Bill1907 <a31829660@gamil.com>
2024-06-09 23:35:53 +0900

    websocket setup

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import WebSocket
 
 app = FastAPI()
 
@@ -6,3 +7,16 @@ app = FastAPI()
 @app.get("/hello")
 def hello() -> dict:
     return {"message": "Hello World"}
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    print('Websocket connection')
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message received: {data}")
+    except Exception as e:
+        print('Error:', e)
+    finally:
+        await websocket.close()
