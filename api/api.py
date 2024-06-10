766f7aeb7df7eb649d8bf8bc6fb961d7c91b9c75
Bill1907 <a31829660@gamil.com>
2024-06-10 23:59:50 +0900

    openai 설정

--- api/api.py
+++ api/api.py
@@ -1,12 +1,28 @@
+import os
 from fastapi import FastAPI
 from fastapi import WebSocket
+from dotenv import load_dotenv
+from openai import OpenAI
 
 app = FastAPI()
 
+load_dotenv()
+key = os.environ.get("OPEN_API_KEY")
 
-@app.get("/hello")
+@app.get("/")
 def hello() -> dict:
-    return {"message": "Hello World"}
+    client = OpenAI(api_key=key)
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"},
+            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+            {"role": "user", "content": "Where was it played?"}
+        ]
+    )
+    return {"message": completion.choices[0].message.content
+}
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
